2ff4a18256313641b3879d931926d4f741560e22
Tahira <tahirazia897@gmail.com>
2024-09-24 10:21:44 -0700

    first commit

---
+++ app/component/Card.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Card = (props) => {
+  return (
+    <div>
+      <h1>name:{props.name}</h1>
+      <h1>age:{props.age}</h1>
+      <h1>classprops.class</h1>
+    </div>
+  )
+}
+
+export default Card
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
-import Hero from "./Components/Nevbar/Hero/Hero"
-import  Nevbar from "./Components/Nevbar/main"
+// import Hero from "./Components/Nevbar/Hero/Hero"
+// import  Nevbar from "./Components/Nevbar/main"
+import Card from "./Component/Card";
+import style from "./page.moudule.css"
 
 export default function myName(){
   return <div>
-  <Nevbar/>
-  <Hero/>  
+  {/* <Nevbar/>
+  <Hero/>  */}
+  <Card/> 
     
     </div>
-}
+// }make 3 routes 
